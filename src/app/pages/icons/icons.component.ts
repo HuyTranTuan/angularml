12d4a292cae9c0440b974b26bef24a24ad6a236d
Huy Tran Tuan <trantuanhuyltv@gmail.com>
2022-08-13 12:59:04 +0700

    report day

--- src/app/pages/icons/icons.component.ts
+++ src/app/pages/icons/icons.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: "app-icons",
   styleUrls: ["icons.component.css"],
@@ -20,6 +21,10 @@ export class IconsComponent implements OnInit {
   public example2: string = '';
   public example3: string = '';
   public example4: string = '';
+  public color1: string = '';
+  public color2: string = '';
+  public color3: string = '';
+  public color4: string = '';
   constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
     this.activatedRoute.queryParams.subscribe(params => {
       this.queryString = params.predict;
@@ -38,6 +43,30 @@ export class IconsComponent implements OnInit {
         this.example2 = `Logistic Regression: ` + this.logistic_regression;
         this.example3 = `SVM: ` + this.svm;
         this.example4 = `LSTM: ` + this.lstm;
+        if(this.naive_bayes == 'thông tin'){ this.color1 = 'class-white' }
+        if(this.naive_bayes == 'hạnh phúc'){ this.color1 = 'class-pink' }
+        if(this.naive_bayes == 'buồn bã'){ this.color1 = 'class-blue' }
+        if(this.naive_bayes == 'ngạc nhiên'){ this.color1 = 'class-green' }
+        if(this.naive_bayes == 'phẫn nộ'){ this.color1 = 'class-red' }
+        if(this.naive_bayes == 'sợ hãi'){ this.color1 = 'class-purple' }
+        if(this.logistic_regression == 'thông tin'){ this.color2 = 'class-white' }
+        if(this.logistic_regression == 'hạnh phúc'){ this.color2 = 'class-pink' }
+        if(this.logistic_regression == 'buồn bã'){ this.color2 = 'class-blue' }
+        if(this.logistic_regression == 'ngạc nhiên'){ this.color2 = 'class-green' }
+        if(this.logistic_regression == 'phẫn nộ'){ this.color2 = 'class-red' }
+        if(this.logistic_regression == 'sợ hãi'){ this.color2 = 'class-purple' }
+        if(this.svm == 'thông tin'){ this.color3 = 'class-white' }
+        if(this.svm == 'hạnh phúc'){ this.color3 = 'class-pink' }
+        if(this.svm == 'buồn bã'){ this.color3 = 'class-blue' }
+        if(this.svm == 'ngạc nhiên'){ this.color3 = 'class-green' }
+        if(this.svm == 'phẫn nộ'){ this.color3 = 'class-red' }
+        if(this.svm == 'sợ hãi'){ this.color3 = 'class-purple' }
+        if(this.lstm == 'thông tin'){ this.color4 = 'class-white' }
+        if(this.lstm == 'hạnh phúc'){ this.color4 = 'class-pink' }
+        if(this.lstm == 'buồn bã'){ this.color4 = 'class-blue' }
+        if(this.lstm == 'ngạc nhiên'){ this.color4 = 'class-green' }
+        if(this.lstm == 'phẫn nộ'){ this.color4 = 'class-red' }
+        if(this.lstm == 'sợ hãi'){ this.color4 = 'class-purple' }
       });
     }
   }
@@ -59,6 +88,30 @@ export class IconsComponent implements OnInit {
         this.example2 = `Logistic Regression: ` + this.logistic_regression;
         this.example3 = `SVM: ` + this.svm;
         this.example4 = `LSTM: ` + this.lstm;
+        if(this.naive_bayes == 'thông tin'){ this.color1 = 'class-white' }
+        if(this.naive_bayes == 'hạnh phúc'){ this.color1 = 'class-pink' }
+        if(this.naive_bayes == 'buồn bã'){ this.color1 = 'class-blue' }
+        if(this.naive_bayes == 'ngạc nhiên'){ this.color1 = 'class-green' }
+        if(this.naive_bayes == 'phẫn nộ'){ this.color1 = 'class-red' }
+        if(this.naive_bayes == 'sợ hãi'){ this.color1 = 'class-purple' }
+        if(this.logistic_regression == 'thông tin'){ this.color2 = 'class-white' }
+        if(this.logistic_regression == 'hạnh phúc'){ this.color2 = 'class-pink' }
+        if(this.logistic_regression == 'buồn bã'){ this.color2 = 'class-blue' }
+        if(this.logistic_regression == 'ngạc nhiên'){ this.color2 = 'class-green' }
+        if(this.logistic_regression == 'phẫn nộ'){ this.color2 = 'class-red' }
+        if(this.logistic_regression == 'sợ hãi'){ this.color2 = 'class-purple' }
+        if(this.svm == 'thông tin'){ this.color3 = 'class-white' }
+        if(this.svm == 'hạnh phúc'){ this.color3 = 'class-pink' }
+        if(this.svm == 'buồn bã'){ this.color3 = 'class-blue' }
+        if(this.svm == 'ngạc nhiên'){ this.color3 = 'class-green' }
+        if(this.svm == 'phẫn nộ'){ this.color3 = 'class-red' }
+        if(this.svm == 'sợ hãi'){ this.color3 = 'class-purple' }
+        if(this.lstm == 'thông tin'){ this.color4 = 'class-white' }
+        if(this.lstm == 'hạnh phúc'){ this.color4 = 'class-pink' }
+        if(this.lstm == 'buồn bã'){ this.color4 = 'class-blue' }
+        if(this.lstm == 'ngạc nhiên'){ this.color4 = 'class-green' }
+        if(this.lstm == 'phẫn nộ'){ this.color4 = 'class-red' }
+        if(this.lstm == 'sợ hãi'){ this.color4 = 'class-purple' }
       });
     }
   }
--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -10,6 +10,15 @@ export class TablesComponent implements OnInit {
   public data: any;
   public dataTrain: any;
   public dataTest: any;
+  public dataLabel = [
+    ['Hạnh phúc', 1919],
+    ['Buồn bã', 1107],
+    ['Phẫn nộ', 1459],
+    ['Sợ hãi', 648],
+    ['Ngạc nhiên', 662],
+    ['Thông tin', 1573],
+  ]
+  public dataEx = [];
   
   constructor(private http: HttpClient) {}
 
@@ -17,6 +26,38 @@ export class TablesComponent implements OnInit {
     this.http.get('http://127.0.0.1:5000/home').subscribe((res: any)=> {
       this.dataTrain = res.array_train.slice(0, 10);
       this.dataTest = res.array_test.slice(0, 10);
+      let hp = [];
+      let bb = [];
+      let sh = [];
+      let pn = [];
+      let nn = [];
+      let tt = [];
+      for (let i of res.array_train){
+        if (i[1] === 'hạnh phúc'){
+          hp.push(i)
+        }
+        if (i[1] === 'buồn bã'){
+          bb.push(i)
+        }
+        if (i[1] === 'sợ hãi'){
+          sh.push(i)
+        }
+        if (i[1] === 'phẫn nộ'){
+          pn.push(i)
+        }
+        if (i[1] === 'ngạc nhiên'){
+          nn.push(i)
+        }
+        if (i[1] === 'thông tin'){
+          tt.push(i)
+        }
+      }
+      for(let i of hp.slice(0, 2)){this.dataEx.push(i);}
+      for(let i of bb.slice(0, 2)){this.dataEx.push(i);}
+      for(let i of sh.slice(0, 2)){this.dataEx.push(i);}
+      for(let i of pn.slice(0, 2)){this.dataEx.push(i);}
+      for(let i of nn.slice(0, 2)){this.dataEx.push(i);}
+      for(let i of tt.slice(0, 2)){this.dataEx.push(i);}
     });
   }
 }
